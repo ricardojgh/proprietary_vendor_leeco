c26e852420bc597207c9bb9885c7813290d96cf1
canossi@gmail.com <canossi@gmail.com>
2019-05-21 04:48:04 +0000

    Revert "Revert "msm8996-common: implement binderized consumerir HAL + bridge""
    This reverts commit 698a7c7dedf04123d0f59e5a462e28ba70d75bab.

--- msm8996-common/Android.mk
+++ msm8996-common/Android.mk
@@ -173,6 +173,7 @@ LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+<<<<<<< HEAD
 LOCAL_MODULE := WfdCommon
 LOCAL_MODULE_OWNER := leeco
 LOCAL_SRC_FILES := proprietary/framework/WfdCommon.jar
@@ -180,6 +181,17 @@ LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := JAVA_LIBRARIES
 LOCAL_MODULE_SUFFIX := .jar
+=======
+LOCAL_MODULE := UEIQuicksetSDKLeTV
+LOCAL_MODULE_OWNER := leeco
+LOCAL_SRC_FILES := proprietary/vendor/app/UEIQuicksetSDKLeTV/UEIQuicksetSDKLeTV.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_VENDOR_MODULE := true
+>>>>>>> parent of 698a7c7... Revert "msm8996-common: implement binderized consumerir HAL + bridge"
 include $(BUILD_PREBUILT)
 
 endif
